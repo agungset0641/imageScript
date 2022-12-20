ca56ee4636d6c756885de496f445fef49b64df6d
Agung Setiawan <8703389@gmail.com>
2022-12-20 13:25:39 +0700

    download 1 file saja

--- downloadimage.js
+++ downloadimage.js
@@ -1,18 +1,23 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 saveImage = async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     page.setViewport({ width: 1366, height: 768, deviceScaleFactor: 1 });
-    await page.goto("https://www.wikipedia.org/");
+    await page.goto("https://officesnapshots.com/2022/12/07/algorand-offices-boston/");
+    await page.evaluate(() => {
+        window.scrollTo(0, window.document.body.scrollHeight);
+      });
 
-    const image = await page.waitForSelector('img[src][alt="Wikipedia"]')
-    const imgURL = await image.evaluate(img => img.getAttribute('src'))
+    const image = await page.waitForSelector('.gallery-item.product-test.normal-mode img[src]', {timeout: 0, waitUntil: 'networkidle0'})
+    const imgURL = await image.evaluate(img => img.getAttribute('data-src'))
     const pageNew = await browser.newPage()
 
-    const response = await pageNew.goto("https://www.wikipedia.org/" + imgURL, {timeout: 0, waitUntil: 'networkidle0'})
+    const response = await pageNew.goto(imgURL, {timeout: 0, waitUntil: 'networkidle0'})
     const imageBuffer = await response.buffer()
-    await fs.promises.writeFile('./logo.png', imageBuffer)
+    
+    await fs.promises.writeFile('./logo1.png', imageBuffer)
     await browser.close()
 }
 
